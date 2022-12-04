a1d6b974e12fe468450a0001a80031a90b25274e
Jakub Pilimon <jpilimon@jpilimon72LW9.vmware.com>
2022-12-04 11:30:17 -0800

    maintenance

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,15 @@
 import java.time.Duration;
 import java.time.Instant;
 import java.util.UUID;
-import java.util.concurrent.TimeUnit;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
 }
-
+//1 resevation
+//2 expiration
+//3 maintance
 class Reservation {
 
     enum Status {
@@ -34,7 +35,7 @@ class Reservation {
         status = Status.Cancelled;
     }
 
-    public boolean ownedBy(RiderId riderId) {
+    boolean ownedBy(RiderId riderId) {
         return this.riderId.equals(riderId);
     }
 }
@@ -51,18 +52,43 @@ class ReservationRepository {
 
 class EScooterService {
 
+
+    final EScooterRepository eScooterRepository;
     final ReservationRepository reservationRepository;
 
-    EScooterService(ReservationRepository reservationRepository) {
+    EScooterService(EScooterRepository eScooterRepository, ReservationRepository reservationRepository) {
+        this.eScooterRepository = eScooterRepository;
         this.reservationRepository = reservationRepository;
     }
 
-    boolean reserve(EScooterId eScooterId, RiderId riderid) {
-        if(reservedBySomeoneElse(eScooterId, riderid)) {
+    boolean putIntoMaintenance(EScooterId eScooterId) {
+        if(isReserved(eScooterId)) {
             return false;
         }
+        EScooter eScooter = eScooterRepository.findByEScooterId(eScooterId);
+        eScooter.putIntoMaintenance();
+        eScooterRepository.save(eScooter);
+        return true;
+    }
+
+    private boolean isReserved(EScooterId eScooterId) {
+        return reservationRepository.findByEscooterId(eScooterId) != null;
+    }
+
+    boolean reserve(EScooterId eScooterId, RiderId riderid) {
+        if (reservedBySomeoneElse(eScooterId, riderid)) {
+            return false;
+        }
+        if (isInMaintenance(eScooterId)) {
+            return false;
+        }
+
         reservationRepository.save(new Reservation(riderid, eScooterId, tenMinutes()));
         return true;
+    }
+
+    private boolean isInMaintenance(EScooterId eScooterId) {
+        return eScooterRepository.findByEScooterId(eScooterId).isInMaintenance();
     }
 
     private Instant tenMinutes() {
@@ -81,20 +107,47 @@ class RiderId {
     static RiderId newOne() {
         return new RiderId(UUID.randomUUID());
     }
+
     private final UUID no;
 
     private RiderId(UUID no) {
         this.no = no;
     }
 }
+
 class EScooterId {
 
     static EScooterId newOne() {
         return new EScooterId(UUID.randomUUID());
     }
+
     private final UUID no;
 
     private EScooterId(UUID no) {
         this.no = no;
+    }
+}
+
+class EScooter {
+
+    private EScooterId eScooterId;
+    private boolean maintanece;
+
+    boolean isInMaintenance() {
+        return maintanece;
+    }
+
+    void putIntoMaintenance() {
+        maintanece = false;
+    }
+}
+
+class EScooterRepository {
+    void save(EScooter eScooter) {
+
+    }
+
+    EScooter findByEScooterId(EScooterId eScooterId) {
+        return null;
     }
 }
